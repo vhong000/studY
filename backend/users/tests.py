8e861cb7f99965c973f1d0b1d3351f0359bb4a0c
Moustafa <drsh2k19@gmail.com>
2018-10-18 13:46:50 -0400

    add basic test

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -10,3 +10,7 @@ class BasicTestCase(TestCase):
     def test_basic(self):
         res = self.client.get('/')
         self.assertEquals(res.status_code, 200)
+
+    def test_api_access(self):
+        res = self.client.get('/api/auth/user')
+        self.assertEquals(res.status_code, 401)
