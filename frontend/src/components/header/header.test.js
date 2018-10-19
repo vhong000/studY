7b81829c26b59e68d2bef3d3142a6efb918266a8
Victor Hong <victorhong04@gmail.com>
2018-10-19 01:18:47 -0400

    header and register/login skeletons

--- frontend/src/components/header/header.test.js
+++ frontend/src/components/header/header.test.js
@@ -1,13 +1,10 @@
 
 import React from 'react';
-import { MemoryRouter, Route } from 'react-router-dom';
 import * as enzyme from 'enzyme';
-import { shallow, mount } from 'enzyme';
-import renderer from 'react-test-renderer';
-import { render } from 'react-dom';
+import { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Button from '@material-ui/core/Button';
 import Header from './header';
+import Login from '../login/login';
 
 enzyme.configure({ adapter: new Adapter() });
 
@@ -15,18 +12,13 @@ describe('Header', () => {
 	const wrapper = shallow(<Header />);
 
 	test('test login button', () => {
-		render(
-			<MemoryRouter>
-				<Header />
-			</MemoryRouter>
-		);
-		Header.find('.register').simulate('click');
-		expect(global.window.location.pathname).toEqual('/register');
+		wrapper.find('.login-button').simulate('click');
+		expect(<Login />)
 	})
 
 	test('test register button', () => {
-		wrapper.find('.login').simulate('click');
-		expect(global.window.location.pathname).toEqual('/login');
+		wrapper.find('.register-button').simulate('click');
+		expect(<Header />);
 
 	})
 })
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Login from './components/login/login';
+import Register from './components/register/register';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 	<Router>
 		<div>
 			<Route exact path="/" component={App} />
-			<Route exact path="/register" component={App} />
-			<Route exact path="/login" component={App} />
+			<Route exact path="/register" component={Register} />
+			<Route exact path="/login" component={Login} />
 		</div>
 	</Router>,
 	document.getElementById('root')
