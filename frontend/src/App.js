1990aa2b2568da25ffb418ba59e177f8a1f00b73
Victor Hong <victorhong04@gmail.com>
2018-10-19 21:25:03 -0400

    auth context store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,31 @@ import logo from './logo.svg';
 import Header from './components/header/header';
 import './App.css';
 
+export const AuthContext = React.createContext();
+
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.initialState.bind(this);
+	}
+
+	componentDidMount() {
+		this.setState(this.initialState());
+	}
+
+	initialState() {
+		const currToken = localStorage.getItem("token");
+		if (!currToken) { return { token: '' }; }
+		return { token: currToken };
+	}
+
   render() {
     return (
-      <div className="App">
-				<Header />
-      </div>
+			<AuthContext.Provider value={this.state}>
+				<div className="App">
+					<Header />
+				</div>
+			</AuthContext.Provider>
     );
   }
 }
