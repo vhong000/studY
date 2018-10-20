1203d68e2c8aac90ce9e1c51c5e492277d7615ef
Victor Hong <victorhong04@gmail.com>
2018-10-19 23:18:48 -0400

    simple token handling using react context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,31 +2,12 @@ import React, { Component } from 'react';
 import { Header } from './components';
 import './App.css';
 
-export const AuthContext = React.createContext();
-
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.initialState.bind(this);
-	}
-
-	componentDidMount() {
-		this.setState(this.initialState());
-	}
-
-	initialState() {
-		const currToken = localStorage.getItem("token");
-		if (!currToken) { return { token: '' }; }
-		return { token: currToken };
-	}
-
   render() {
     return (
-			<AuthContext.Provider value={this.state}>
-				<div className="App">
-					<Header />
-				</div>
-			</AuthContext.Provider>
+			<div className="App">
+				<Header />
+			</div>
     );
   }
 }
