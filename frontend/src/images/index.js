4ed90f29a474bf73c4fbcedc36a718b579209643
Moustafa <drsh2k19@gmail.com>
2018-10-21 14:56:06 -0400

    Revert "landing page mockup #22"

--- frontend/src/images/index.js
+++
@@ -1,7 +0,0 @@
-
-export { default as math } from './math_card.jpg';
-export { default as science } from './science_card.png';
-export { default as literature } from './literature_card.jpeg';
-export { default as history } from './history_card.jpeg';
-export { default as art } from './art_card.jpeg';
-export { default as language } from './language_card.jpeg';
