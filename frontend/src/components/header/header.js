979a7dfc222f17d4ea3ff8cbfe2a74fcc5f017fb
Victor Hong <victorhong04@gmail.com>
2018-10-21 19:53:23 -0400

    context refactor and login adjustments

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -5,51 +5,47 @@ import {
 	Button, AppBar, Typography, Toolbar,
 } from '@material-ui/core';
 
-import { AuthConsumer } from '../../contexts/Auth.context.js';
+import { AuthWrapper } from '../../contexts/Auth.context.js';
 
 class Header extends Component {
 
 	render() {
 		return (
-			<AuthConsumer>
-				{context => (
-					<AppBar position='static' >
-						<Toolbar>
-							<Typography	
-								style={{flex: 1}}
-								variant='headline'
-								color='inherit'>
-								studY {context.token}
-							</Typography>
-						{context.token === '' ? (
-							<div>
-								<Button
-									component={Link}
-									to="/login"
-									className='login-button'
-									children="Login"
-							color='inherit' />
-							<Button 
-								component={Link} 
-								to="/register"
-								className='register-button'
-								color='inherit'
-								children="Register"
-							/>
-							</div>
-						) : (
-							<Button 
-								color='inherit'
-								children="logout"
-								onClick={context.clear}
-							/>
-						)}
-					</Toolbar>
-				</AppBar>
-			)}
-		</AuthConsumer>
+			<AppBar position='static' >
+				<Toolbar>
+					<Typography	
+						style={{flex: 1}}
+						variant='headline'
+						color='inherit'>
+						studY {this.props.token}
+					</Typography>
+				{this.props.token === '' ? (
+					<div>
+						<Button
+							component={Link}
+							to="/login"
+							className='login-button'
+							children="Login"
+					color='inherit' />
+					<Button 
+						component={Link} 
+						to="/register"
+						className='register-button'
+						color='inherit'
+						children="Register"
+					/>
+					</div>
+				) : (
+					<Button 
+						color='inherit'
+						children="logout"
+						onClick={this.props.clear}
+					/>
+				)}
+			</Toolbar>
+		</AppBar>
 		)
 	}
 }
 
-export default Header;
+export default AuthWrapper(Header);
