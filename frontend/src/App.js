8769593548e32179e543ae52d11ceb3ca28ae578
Victor Hong <victorhong04@gmail.com>
2018-10-21 13:15:28 -0400

    display topics in landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,92 @@
 import React, { Component } from 'react';
 import { Header } from './components';
+import {
+	Paper, Card, CardContent, CardActions,
+	Typography, Grid, withStyles, CardMedia,
+	CardActionArea,
+} from '@material-ui/core';
 import './App.css';
+
+import { 
+	math, science, history, art, literature, language,
+} from './images'
+
+const topics = [
+	{ 
+		name: "Math",
+		image: math,
+	},
+	{ 
+		name: "History",
+		image: history,
+	},
+	{ 
+		name: "Language",
+		image: language,
+	},
+	{ 
+		name: "Science",
+		image: science,
+	},
+	{ 
+		name: "Art",
+		image: art,
+	},
+	{ 
+		name: "Literature",
+		image: literature,
+	},
+]
+
+const styles = {
+	main_grid: {
+		"margin-top": 20,
+	},
+	card: {
+	},
+	media: {
+		height: 200,
+	}
+}
 
 class App extends Component {
   render() {
+		const { classes } = this.props;
     return (
-			<div className="App">
+			<div className='App'>
 				<Header />
+
+				<Grid className={classes.main_grid} container justify='center' >
+					<Grid 
+						className={classes.topics_grid}
+						container
+						xs='6'
+						spacing='24'
+					>
+						{topics.map((topic) => {
+							return (
+								<Grid item xs='6' >
+									<Card className={classes.card}>
+										<CardActionArea>
+											<CardMedia 
+												className={classes.media}
+												image={topic.image}
+											/>
+											<CardContent>
+												<Typography variant="h6">
+													{topic.name}
+												</Typography>
+											</CardContent>
+										</CardActionArea>
+									</Card>
+								</Grid>
+							)})
+							}
+					</Grid>
+				</Grid>
 			</div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
