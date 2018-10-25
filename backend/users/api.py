6e2ccb8f02bf2fe5effcbcc727c2a595a9f8c18f
Moustafa <drsh2k19@gmail.com>
2018-10-25 00:26:46 -0400

    update settings module to use env vars in prod

--- backend/users/api.py
+++ backend/users/api.py
@@ -1,10 +1,3 @@
-import json
-
-# from django.conf import settings
-# from django.db.models import Q
-# from rest_framework import generics
-# from django.contrib.auth.models import AnonymousUser
-from django.contrib.auth.models import User
 from rest_framework.authentication import (BasicAuthentication,
                                            SessionAuthentication,
                                            TokenAuthentication)
@@ -15,9 +8,11 @@ from rest_framework.parsers import JSONParser, FormParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from .models import Student
+from django.contrib.auth.models import User
 from .serializers import StudentSerializer
+from .models import Student
 import uuid
+import json
 
 
 class SignupView(APIView):
