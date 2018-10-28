3c3cebe9d0dc4447e3a7624143bb763ff5277f23
Victor Hong <victorhong04@gmail.com>
2018-10-28 14:28:12 -0400

    banner, button, and images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,15 @@ import { Link } from 'react-router-dom';
 import {
 	Card, CardContent, Typography, 
 	Grid, withStyles, CardMedia,
-	CardActionArea,
+	CardActionArea, Button
 } from '@material-ui/core';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
 import './App.css';
 
 import { 
-	math, science, history, art, literature, language,
+	math, science, history, art, literature, language, study_banner,
+	study_banner1, study_banner2
 } from './images'
 
 const topics = [
@@ -38,33 +41,70 @@ const topics = [
 	},
 ]
 
-const styles = {
+const styles = theme => ({
 	main_grid: {
 		"margin-top": 20,
 	},
 	card: {
 	},
+	banner_button: {
+		position: 'absolute',
+		width: '25%',
+		top: '40%',
+		left: '50%',
+		backgroundColor: theme.palette.primary.dark,
+		'&:hover': {
+			backgroundColor: theme.palette.primary.dark,
+		},
+		transform: 'translate(-50%, 50%)',
+		color: 'white'
+	},
 	media: {
 		height: 200,
+	},
+	banner: {
+		height: 500,
+		width: '100%',
+		"object-fit": 'cover'
 	}
-}
+})
+
+const banners = [ study_banner, study_banner1, study_banner2 ];
+const Banner = autoPlay(SwipeableViews);
 
 class App extends Component {
   render() {
-		const { classes } = this.props;
+		const { classes, theme } = this.props;
     return (
 			<div className='App'>
-
+				<div className='banner-container'>
+					<Banner
+					axis='x'
+					autoplay interval={10000} >
+						{banners.map((step, index) => (
+							<div key={index}>
+								<img className={classes.banner}
+								src={step}
+								alt='banner' />
+							</div>
+						))}
+					</Banner>
+					<Button 
+						className={classes.banner_button}
+						children="Sign Up"
+						variant='text'
+					/>
+				</div>
 				<Grid className={classes.main_grid} container justify='center' >
 					<Grid 
 						className={classes.topics_grid}
 						container item
-						xs='6'
+						xs='8'
 						spacing='24'
 					>
 						{topics.map((topic) => {
 							return (
-								<Grid item xs='6' >
+								<Grid item xs='12' md='6' lg='4' >
 									<Card className={classes.card}>
 										<CardActionArea
 											component={Link}
