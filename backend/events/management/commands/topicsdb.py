4f840b21472b82f0e474a968ea67f1b826b5b7c5
Cng000 <carlosngsang.cs@gmail.com>
2018-11-02 19:04:28 -0400

    Added category and topics

--- backend/events/management/commands/topicsdb.py
+++ backend/events/management/commands/topicsdb.py
@@ -1,39 +1,27 @@
 from django.core.management.base import BaseCommand
-from events.models import School
+from events.models import Category
 import json
 from pathlib import Path
-
+import sys
+from itertools import chain
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
             '--dirpath',
             type=str,
-            help='./backend/manage.py populatedb --dirpath ./backend/events/data',
+            help='./backend/manage.py topicsdb --dirpath ./backend/events/data/topics',
         )
 
     def handle(self, *args, **options):
-        DUMP_DIR = Path(options['dirpath'])
-        if not DUMP_DIR.is_dir():
-            raise FileNotFoundError()
+        file = sys.argv[-1]
+        with open(file) as fh:
+            categories = json.load(fh)
 
-        category= []
-        for file_ in DUMP_DIR.glob('*.json'):
-            with file_.open() as fh:
-                category.append(json.load(fh))
+        for cat, obj in categories.items():
+            category, created = Category.objects.get_or_create(name=cat)
+            topics = list(chain.from_iterable(obj.values()))
 
-        for in category:
-            new_school, created = School.objects.get_or_create(name=sch['name'],
-                                                               code=sch['code'])
-            if 'courses' not in sch:
-                continue
-            for dept, courses in sch['courses'].items():
-                for course in courses:
-                    try:
-                        new_school.courses.get_or_create(name=course[0],
-                                                         number=''.join(
-                                                             filter(lambda c: c.isdigit(), courses[1])),
-                                                         dept=dept)
-                    except Exception as e:
-                        # print(e)
-                        pass
+            for topic in topics:
+                category.topics.get_or_create(name=topic[0])
+
