1c219443eddcc2c669c64d2ce47be3de3f60b231
Cng000 <carlosngsang.cs@gmail.com>
2018-11-04 15:11:55 -0500

    wrote first test

--- backend/events/tests.py
+++ backend/events/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
+from events.models import *
 
 # Create your tests here.
+
+class SchoolTest(TestCase):
+    def create_school(self,name="City College",code="CCNY"):
+        return School.objects.create(name=name,code=code)
+
+    def test_create_school(self):
+        school = self.create_school()
+        self.assertTrue(isinstance(school,School))
+        self.assertEqual(school.__str__(),"City College - CCNY")
+
+class
