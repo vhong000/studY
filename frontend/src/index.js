b1feb0ce1893036a692872d01c5094f35a1b1a42
Victor Hong <victorhong04@gmail.com>
2018-11-06 02:04:36 -0500

    removed mock data

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,53 +5,11 @@ import './index.css';
 // import App from './App';
 import EventForm from './containers/eventForm';
 // import SubtopicPage from './components/SubtopicPage/SubtopicPage';
-import { EventListPage, 
-	Header, Register, Login, 
+import { EventListPage, Header,
 	EventPage, SubtopicPage, App } from './containers';
 import * as serviceWorker from './serviceWorker';
 
 import { AuthProvider } from './contexts/Auth.context.js';
-
-import {
-	math, science, history, art, literature, language,
-} from './images'
-
-const subtopics = {
-	math: {
-		titles: ["Precalculus", "Calculus I", "Calculus II", "Calculus III", "Probability and Statistics",
-			"Linear Algebra", "Differential Equations", "Mathematical Statistics", "Numerical Analysis",
-			"Geometry", "Financial Mathematics", "Set Theory", "Pure Mathematics", "Classical Analysis",
-			"Mathematical Logic", "Topology", "Probability Theory", "Fourier Analysis", "Differential Geometry",
-			"Optimization Theory"],
-		image: math
-	},
-	history: {
-		titles: ["The United States: From Its Origins to 1877", "The United States: Since 1865", "Latin America in World History",
-			"Colonial Latin America", "Modern and Contemporary Latin America", "History of U.S. Immigration",
-			"The Ancient World: The Near East and Greece", "The Ancient World: Rome", "Early-Modern Europe",
-			" Modern Europe", " Medieval Europe", "European Union", "The French Revolution", " The Age of Enlightenment",
-			"The Middle East in Global History", "The Middle East Under Islam", "Africa and the Modern World", "The Long Partition: India & Pakistan",
-			"The Era of the American Revolution", "The Era of Civil War and Reconstruction, 1840-1877"],
-		image: history
-	},
-	language: {
-		titles: ["English I", "English II", "English III", "French I", "French II", "French III", "Spanish I", "Spanish II", "Spanish III",
-			"Italian I", "Italian II", "Italian III", "German I", "German II", "German III", "Chinese", "Japanese", "Russian", "Arabic", "Hindi"],
-		image: language
-	},
-	science: {
-		titles: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"],
-		image: science
-	},
-	art: {
-		titles: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"],
-		image: art
-	},
-	literature: {
-		titles: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"],
-		image: literature
-	},
-}
 
 ReactDOM.render(
 		<Router>
