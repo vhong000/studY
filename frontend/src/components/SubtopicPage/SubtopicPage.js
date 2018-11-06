8887f77742f6351ab01ddf7acda073867f0c878d
Rohan Swaby <rohan_swaby51@yahoo.com>
2018-11-06 12:44:37 -0500

    bug fix

--- frontend/src/components/SubtopicPage/SubtopicPage.js
+++ frontend/src/components/SubtopicPage/SubtopicPage.js
@@ -37,7 +37,7 @@ class SubtopicPage extends Component {
 									<Card className={classes.card}>
 										<CardActionArea
 											component={Link}
-											to={`/${topic.name}/${topic.id}`} >
+											to={`/${category}/${topic.id}`} >
 											<CardMedia 
 												className={classes.media}
 												image={topic.image} />
