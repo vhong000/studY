d6df225779e5afda03746ec04291339f7745c7c4
Hanna Nekhniadovich <nekhanya@gmail.com>
2018-11-16 20:25:45 -0500

    profile page initial styling

--- frontend/src/components/UserProfilePage/UserProfilePage.js
+++ frontend/src/components/UserProfilePage/UserProfilePage.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import { Typography, Grid, withStyles, Button } from '@material-ui/core';
 import styles from './UserProfilePage.styles';
+import icon from '../../images/icon.jpg';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import AlternateEmailIcon from '@material-ui/icons/AlternateEmail';
 
 class UserProfilePage extends Component {
    
     render() {
         const { classes, user, school } = this.props;
         return (
+            <>
+            <div style={{height: "160px", backgroundColor: "rgb(148, 160, 231)"}}>
+            {/* <Grid container justify="center"> */}
+
+                <Grid container direction="column" className={classes.header_grid}>
+                    <Grid item> 
+                        <Typography variant="subtitle1" gutterBottom className={classes.header_typography}>
+                            <PersonOutlineIcon className={classes.iconUi}/>&ensp;
+                            {`${user.owner.first_name} ${user.owner.last_name}`}
+                    </Typography>
+                    </Grid>
+                    <Grid>
+                        <Typography variant="subtitle1" gutterBottom className={classes.header_typography}>
+                            <AlternateEmailIcon className={classes.iconUi}/>&ensp;
+                            {`${user.owner.email}`}
+                    </Typography>
+                    </Grid>
+                </Grid>
+            {/* </Grid> */}
+            </div>
+            <img alt="icon" src={icon} className={classes.icon}/> 
             <Grid container spacing={24} className={classes.main_grid}>
                 <Grid item xs={10}>
                     <Typography variant="subtitle1" gutterBottom>
@@ -27,6 +51,7 @@ class UserProfilePage extends Component {
                     <Button>Edit</Button>
                 </Grid>
             </Grid>
+            </>
         )
     }
 }
--- frontend/src/components/UserProfilePage/UserProfilePage.styles.js
+++ frontend/src/components/UserProfilePage/UserProfilePage.styles.js
@@ -1,7 +1,30 @@
 const styles = {
     main_grid: {
         "margin": 20,   
+    },
+    icon: {
+        position: "fixed",
+        top: "145px",
+        left: "100px",
+        height: "150px",
+        width: "150px",
+        borderRadius: "75px"
+    },
+    iconUi: {
+        color: "white",
+        verticalAlign: "text-bottom",
+    }, 
+    header_grid: {
+        marginLeft: "25%",
+        paddingTop: "50px"
+    },
+    header_typography: {
+        color: "white",
+        fontSize: "18px",
+        fontFamily: "Raleway",
+        fontWeight: "600"
     }
+
 }
     
 export default styles;
