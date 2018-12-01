ebdc6896fe2d6ed8226a9681e278816622f4f7c3
Cng000 <carlosngsang.cs@gmail.com>
2018-12-01 13:03:53 -0500

    added deployed db fix, and added superuser

--- backend/post_release.sh
+++ backend/post_release.sh
@@ -3,9 +3,11 @@ EXEC='python manage.py'
 $EXEC makemigrations
 $EXEC migrate
 cnt=$(sh -c "$EXEC shell -c 'from events.models import School; print(School.objects.count())'")
+user=$(sh -c "$EXEC shell -c 'from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@gmail.com', 'studYpassword')'")
 if [ $cnt -lt 10 ]
 then
     echo $cnt
     $EXEC populatedb --dirpath events/data/
-    $EXEC topicsdb --dirpath events/data/topics/topics.json
 fi
+echo $user
+$EXEC topicsdb --dirpath events/data/topics/topics.json
