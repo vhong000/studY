b6e07ad2ecb0d994c00d69a337aaf8dc84eab9b3
Rohan Swaby <rohan_swaby51@yahoo.com>
2018-12-02 00:42:27 -0500

    register snapshot

--- frontend/src/components/register/register.test.js
+++ frontend/src/components/register/register.test.js
@@ -4,6 +4,7 @@ import React from 'react';
 import * as enzyme from 'enzyme';
 import { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
 
 import Register from './register';
 
@@ -27,12 +28,22 @@ describe('Register component', () => {
 	const wrapper = mount(<Register handleSubmit={mock} classes={styles} />);
 
 	it('should render all fields', () => {
-    expect(wrapper.find('#first_name').exists()).toBe(true);
-    expect(wrapper.find('#last_name').exists()).toBe(true);
-    expect(wrapper.find('#email').exists()).toBe(true);
-    expect(wrapper.find('#password').exists()).toBe(true);
-    expect(wrapper.find('#school').exists()).toBe(true);
-    expect(wrapper.find('#major').exists()).toBe(true);
+		expect(wrapper.find('#first_name').exists()).toBe(true);
+		expect(wrapper.find('#last_name').exists()).toBe(true);
+		expect(wrapper.find('#email').exists()).toBe(true);
+		expect(wrapper.find('#password').exists()).toBe(true);
+		expect(wrapper.find('#school').exists()).toBe(true);
+		expect(wrapper.find('#major').exists()).toBe(true);
+
+
+	})
+
+	test('snapshot', () => {
+		///
+		const component = renderer.create(<Register handleSubmit={mock} classes={styles} />);
+		const tree = component.toJSON();
+		expect(tree).toMatchSnapshot();
+
 	})
 
 	// test('function call on submit', () => {
